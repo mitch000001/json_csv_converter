be82728f53a5d9679584a5f88f6d8907491ffa37
Michael Wagner <mitch.wagna@gmail.com>
2013-04-23 16:52:08 +0200

    Added test data for nesting, also mock for test

---
+++ spec/nested_file_conversion_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+describe JSONCSVConverter::Converter do
+
+  it 'should take several files' do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == true
+  end
+end
