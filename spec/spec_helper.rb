1bcb59e71ab47d8d6a3b41ca5c5cd805d1b82586
Michael Wagner <mitch.wagna@gmail.com>
2013-09-27 22:13:04 +0200

    Add RSpec configuration options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,14 @@ require 'simplecov'
 SimpleCov.start
 require 'rspec'
 require 'json_csv_converter'
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
