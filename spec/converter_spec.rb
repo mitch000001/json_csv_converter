53d28a35b7622eee1353e2214f44dd9ee3c4a678
Michael Wagner <mitch.wagna@gmail.com>
2013-09-27 22:13:38 +0200

    Change testing for JSON to JSON objects

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
+require 'json'
 
 describe JSONCSVConverter::Converter do
-  
+
   let(:converter) do
-    JSONCSVConverter::Converter.new("spec/fixtures/tests.json","spec/fixtures/test.csv","spec/fixtures/test_mapping.yml")
+    JSONCSVConverter::Converter.new("spec/fixtures/tests.json",
+                                    "spec/fixtures/test.csv",
+                                    "spec/fixtures/test_mapping.yml")
   end
-  
+
   subject {converter}
-  
+
   it {should_not be_nil}
   it {should respond_to(:csv_table)}
   it {should respond_to(:json)}
@@ -15,7 +18,7 @@ describe JSONCSVConverter::Converter do
   it {should respond_to(:csv_to_json)}
 
   describe "migrate data from json to csv" do
-    
+
     it "#csv_table should return the old csv table with the json data appended" do
       expected = <<-EOF
 fohoo,bahaar
@@ -24,14 +27,15 @@ tidy up,41
       EOF
       converter.csv_table.to_csv.should == expected
     end
-    
+
   end
 
   describe "migrate data from csv to json" do
 
     it "#json should return the old json object with the csv data appended" do
-      expected = %q({"tests":[{"foo":3,"bar":"string"},{"foo":"tidy up","bar":41}]})
-      converter.csv_to_json.to_json.should == expected
+      expected = JSON(%q({"tests":[{"foo":3,"bar":"string"},{"foo":"tidy up","bar":41}]}))
+      result_string = converter.csv_to_json.to_json
+      JSON(result_string).should == expected
     end
 
   end
